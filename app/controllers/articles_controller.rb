83e56d7cc0bbef73058141d2e5087d415fcaf4d8
Mathieu VALLEAU <mathieuvalleau@hotmail.fr>
2024-01-30 21:19:38 +0100

    Fonctionnel mais front à travailler

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,10 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def update
     @article = Article.find(params[:id])
     @article.update(article_params)
